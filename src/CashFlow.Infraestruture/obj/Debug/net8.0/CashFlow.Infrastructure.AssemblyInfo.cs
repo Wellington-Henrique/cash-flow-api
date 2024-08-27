099fe2fd8bac73253b7c0a76d8d52f61b8e34b7b
Wellington Henrique <wellingtonhlc@gmail.com>
2024-08-27 20:47:17 -0300

    Injeção de dependência pra Banco de Dados: Criando o repositório para despesas

--- src/CashFlow.Infraestruture/obj/Debug/net8.0/CashFlow.Infrastructure.AssemblyInfo.cs
+++ src/CashFlow.Infraestruture/obj/Debug/net8.0/CashFlow.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87f78d590fb807825fcfd0e3d70a56fc5d8f874e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1df5b04ecbbb104c7428b47e853ac80f43cdce93")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
