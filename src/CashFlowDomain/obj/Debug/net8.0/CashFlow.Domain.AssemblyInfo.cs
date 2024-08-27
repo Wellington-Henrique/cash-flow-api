3eae013fdc4b0162726fb0f8fef71a2f95650453
Wellington Henrique <wellingtonhlc@gmail.com>
2024-08-26 23:01:05 -0300

    Introdução a Banco de Dados: Criando as entidades

--- src/CashFlowDomain/obj/Debug/net8.0/CashFlow.Domain.AssemblyInfo.cs
+++ src/CashFlowDomain/obj/Debug/net8.0/CashFlow.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88d7458a5ddd7449afece5110e403aca936af7a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63fc6d39253ee18bd5cb152f3ec0edd3a6b225de")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
