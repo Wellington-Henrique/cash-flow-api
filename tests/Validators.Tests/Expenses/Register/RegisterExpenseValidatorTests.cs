63fc6d39253ee18bd5cb152f3ec0edd3a6b225de
Wellington Henrique <wellingtonhlc@gmail.com>
2024-08-26 22:25:05 -0300

    Testes de unidade: Concluindo os testes para o Validator

--- tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Enums;
 using CashFlow.Exception;
 using CommonTestUtilities.Requests;
 using FluentAssertions;
@@ -34,9 +35,81 @@ namespace Validators.Tests.Expenses.Register
             request.Title = string.Empty;
 
             //Act
-
             var result = validator.Validate(request);
             
+            //Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.TITLE_REQUIRED));
+        }
+
+        [Fact]
+        public void ErrorFutureDate()
+        {
+            //Arrange
+            var validator = new RegisterExpenseValidator();
+            var request = RequestRegisterExpenseJsonBuilder.Build();
+
+            request.Date = DateTime.UtcNow.AddDays(1);
+
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.EXPENSES_CANNOT_FOR_THE_FUTURE));
+        }
+
+        [Fact]
+        public void ErrorInvalidPaymentType()
+        {
+            //Arrange
+            var validator = new RegisterExpenseValidator();
+            var request = RequestRegisterExpenseJsonBuilder.Build();
+
+            request.PaymentType = (PaymentType)5;
+
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.PAYMENT_TYPE_INVALID));
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void ErrorInvalidAmount(decimal amount)
+        {
+            //Arrange
+            var validator = new RegisterExpenseValidator();
+            var request = RequestRegisterExpenseJsonBuilder.Build();
+
+            request.Amount = amount;
+
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO));
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("  ")]
+        [InlineData(null)]
+        public void ErrorInvalidTitle(string title)
+        {
+            //Arrange
+            var validator = new RegisterExpenseValidator();
+            var request = RequestRegisterExpenseJsonBuilder.Build();
+
+            request.Title = title;
+
+            //Act
+            var result = validator.Validate(request);
+
             //Assert
             result.IsValid.Should().BeFalse();
             result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.TITLE_REQUIRED));
