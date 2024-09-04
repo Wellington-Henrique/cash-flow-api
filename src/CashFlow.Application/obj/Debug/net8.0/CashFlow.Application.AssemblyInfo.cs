b260757bff8f885d131e589b85bf431d783182d6
Wellington Henrique <wellingtonhlc@gmail.com>
2024-09-03 21:05:52 -0300

    Gerando relatórios em PDF: Configurando as fontes para o PDF

--- src/CashFlow.Application/obj/Debug/net8.0/CashFlow.Application.AssemblyInfo.cs
+++ src/CashFlow.Application/obj/Debug/net8.0/CashFlow.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ba9cd5febd0e37ecd49b00a3263ee7192d3cd34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b58af693e515266db469dfe4340fcd37235a631")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
