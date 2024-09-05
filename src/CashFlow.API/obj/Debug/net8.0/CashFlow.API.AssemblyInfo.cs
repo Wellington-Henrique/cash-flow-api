70e71562243a1e4351d9cbe6a69c1e3923e0c9fb
Wellington Henrique <wellingtonhlc@gmail.com>
2024-09-05 20:52:20 -0300

    Introdução ao módulo de autenticação: Executando migration pela API

--- src/CashFlow.API/obj/Debug/net8.0/CashFlow.API.AssemblyInfo.cs
+++ src/CashFlow.API/obj/Debug/net8.0/CashFlow.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9c9b1fadccbd8859f665485f6c20f67badacf8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8045aa8a72dd998ece9ee29892367c5e594cb28")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
