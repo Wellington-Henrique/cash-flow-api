9d77b65185a3c0683d22050c2be240836832d5b9
Wellington Henrique <wellingtonhlc@gmail.com>
2024-09-09 23:04:57 -0300

    Autorização nos endpoints: Passando um token pelo Swagger

--- src/CashFlow.API/obj/Debug/net8.0/CashFlow.API.AssemblyInfo.cs
+++ src/CashFlow.API/obj/Debug/net8.0/CashFlow.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fd8b93b36d517942d1fef2e94b186942c12660e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8e014d4162bb7bb6b67d7382324625660130355")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
